000cd055953f300983eb19f3300d9bc3d304e601
younsiamed <younsiamed@gmail.com>
2024-07-02 23:19:29 +0300

    Lesson-5-2

---
+++ Копирование умных массивов/Копирование умных массивов.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <algorithm>
+
+class smart_array {
+private:
+    int size;
+    int* data;
+
+public:
+    smart_array(int sz = 0) : size(sz), data(new int[size]) {}
+
+    smart_array(const smart_array& other) : size(other.size), data(new int[other.size]) {
+        std::copy(other.data, other.data + other.size, data);
+    }
+
+    smart_array& operator=(const smart_array& other) {
+        if (this != &other) {
+            delete[] data;
+
+            size = other.size;
+            data = new int[size];
+            std::copy(other.data, other.data + other.size, data);
+        }
+        return *this;
+    }
+
+    ~smart_array() {
+        delete[] data;
+    }
+
+    void add_element(int element) {
+    }
+
+};
+
+int main() {
+    SetConsoleOutputCP(65001);
+    smart_array arr(5);
+    arr.add_element(1);
+    arr.add_element(4);
+    arr.add_element(155);
+
+    smart_array new_array(2);
+    new_array.add_element(44);
+    new_array.add_element(34);
+
+    arr = new_array;
+
+    return 0;
+}
---
+++ Умный массив/Умный массив.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <stdexcept>
+
+class smart_array {
+public:
+    smart_array(size_t size) : size(size), count(0) {
+        data = new int[size];
+    }
+
+    void add_element(int element) {
+        if (count >= size) {
+            throw std::out_of_range("Массив заполнен. Невозможно добавить больше элементов.");
+        }
+        data[count++] = element;
+    }
+
+    int get_element(size_t index) const {
+        if (index >= count) {
+            throw std::out_of_range("Индекс вне диапазона.");
+        }
+        return data[index];
+    }
+
+    ~smart_array() {
+        delete[] data;
+    }
+
+private:
+    int* data;
+    size_t size;
+    size_t count;
+};
+
+int main() {
+    SetConsoleOutputCP(65001);
+    try {
+        smart_array arr(5);
+        arr.add_element(1);
+        arr.add_element(4);
+        arr.add_element(155);
+        arr.add_element(14);
+        arr.add_element(15);
+        std::cout << arr.get_element(1) << std::endl;
+    }
+    catch (const std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
+    return 0;
+}
